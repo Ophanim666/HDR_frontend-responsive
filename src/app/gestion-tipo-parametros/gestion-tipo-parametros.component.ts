94016951fc0c10a5dcf3e072f173a4b578f9cd42
Ophanim666 <al.arriaza@duocuc.c>
2024-08-22 11:15:53 -0400

    last update

--- src/app/gestion-tipo-parametros/gestion-tipo-parametros.component.ts
+++ src/app/gestion-tipo-parametros/gestion-tipo-parametros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-gestion-tipo-parametros',
