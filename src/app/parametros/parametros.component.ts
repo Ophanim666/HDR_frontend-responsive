1d2072cb7962e05bdb80554d98c0eb02866ca73e
javierasofia <ja.bermudez@duocuc.cl>
2024-10-16 22:20:45 -0300

    Avances siguiendo la estructura de Front Proveedor

--- src/app/parametros/parametros.component.ts
+++ src/app/parametros/parametros.component.ts
@@ -4,15 +4,10 @@ import { NgForm } from '@angular/forms';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
-interface TipoParametro {
-  id: number;
-  tipO_PARAMETRO: string;  // Actualizado para coincidir con la respuesta del API
-}
-
 @Component({
   selector: 'app-parametros',
   templateUrl: './parametros.component.html',
-  styleUrls: ['./parametros.component.css'],
+  styleUrls: ['./parametros.component.css']
 })
 export class ParametrosComponent implements OnInit {
   parametros: any[] = [];
@@ -23,11 +18,9 @@ export class ParametrosComponent implements OnInit {
   searchText: string = '';
   isEditMode = false;
   pagedParametros: any[] = [];
-
-  tipoParametros: TipoParametro[] = [];
-
+  tipoParametros: any[] = [];
   private apiUrl = 'https://localhost:7125/api/Parametro';
-  private apiUrlTipoParametro = 'https://localhost:7125/api/TipoParametro/LstTipoParametros';
+  private apiUrlTipoParametro = 'https://localhost:7125/api/TipoParametro';
 
   showErrorModal = false;
   errorMessage: { message: string, isError: boolean } = { message: '', isError: true };
@@ -41,60 +34,46 @@ export class ParametrosComponent implements OnInit {
     this.loadTipoParametros();
   }
 
-  // Filtrar parámetros
+  loadTipoParametros(): void {
+    this.http.get<any>(`${this.apiUrlTipoParametro}/LstTipoParametros`).subscribe({
+      next: response => {
+        if (response.estado.ack) {
+          this.tipoParametros = response.body.response;
+        } else {
+          this.showError(`Error al cargar los tipos de parámetros: ${response.estado.errDes}`, true);
+        }
+      },
+      error: error => {
+        console.error('Error al cargar los tipos de parámetros:', error);
+        this.showError('Error en la solicitud al cargar los tipos de parámetros.', true);
+      }
+    });
+  }
+
   filteredParametros() {
     return this.parametros.filter(parametro =>
       parametro.parametro.toLowerCase().includes(this.searchText.toLowerCase())
     );
   }
 
-  // Listar o cargar parámetros
   loadParametros(): void {
     this.http.get<any>(`${this.apiUrl}/Listar`).subscribe({
       next: response => {
         if (response.estado.ack) {
           this.parametros = response.body.response;
-          this.updatePagedParametros();
-          console.log('Parámetros cargados:', this.parametros);
-        }else{
-          this.showError(`Error al cargar los parámetros: ${response.estado.errDes}`, true);
+          this.updatePageParametro();
+        } else {
+          this.showError(`Error al cargar los Parámetros: ${response.estado.errDes}`, true);
         }
       },
       error: error => {
         console.error('Error al cargar los datos:', error);
         this.showError('Error en la solicitud al cargar los datos.', true);
-      },
-      complete: () => console.log('Carga de parámetros completa')
-    });
-  }
-
-  // Listar o cargar tipo parámetros
-  loadTipoParametros(): void {
-    this.http.get<any>(this.apiUrlTipoParametro).subscribe({
-      next: response => {
-        if (response.estado.ack) {
-          // Ahora usamos directamente la respuesta sin mapear, ya que coincide con nuestra interfaz
-          this.tipoParametros = response.body.response;
-          console.log('Tipos de parámetros cargados:', this.tipoParametros);
-        } else {
-          this.showError(`Error al cargar los tipos de parámetro: ${response.estado.errDes}`, true);
-        }
-      },
-      error: error => {
-        console.error('Error al cargar los tipos de parámetro:', error);
-        this.showError('Error en la solicitud al cargar los tipos de parámetro.', true);
       }
     });
   }
 
-    // Método helper para obtener el nombre del tipo de parámetro
-    getTipoParametroNombre(id: number): string {
-      const tipo = this.tipoParametros.find(t => t.id === id);
-      return tipo ? tipo.tipO_PARAMETRO : '';
-    }
-
-  // Actualizar los parámetros paginados
-  updatePagedParametros(): void {
+  updatePageParametro(): void {
     const filtered = this.filteredParametros();
     const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
     const endIndex = startIndex + this.paginator.pageSize;
@@ -102,37 +81,32 @@ export class ParametrosComponent implements OnInit {
     this.paginator.length = filtered.length;
   }
 
-   // Método que se llama cuando cambia el texto de búsqueda
   onSearchChange(): void {
     this.paginator.firstPage();
-    this.updatePagedParametros();
+    this.updatePageParametro();
   }
 
-  // Abrir el modal de parámetro
   openModalParametro(parametro?: any): void {
     this.isEditMode = !!parametro;
     this.currentParametro = parametro ? { ...parametro } : {
-      id: 0,
       parametro: '',
       valor: '',
-      tipO_PARAMETRO: null, // Asegúrate de que este nombre coincida con tu modelo
-      estado: 1,
+      tipO_PARAMETRO: '',
+      estado: 1
     };
     this.showModalParametro = true;
+    document.body.classList.add('modal-open');
   }
 
-  // Método que maneja el cambio en el toggle de estado de parámetro
   onToggleChange(event: MatSlideToggleChange): void {
     this.currentParametro.estado = event.checked ? 1 : 0;
   }
 
-  // Cerrar el modal de parámetro
   closeModalParametro(): void {
     this.showModalParametro = false;
     document.body.classList.remove('modal-open');
   }
 
-  // Método para guardar un parámetro
   saveParametro(): void {
     if (this.isEditMode) {
       this.updateParametro();
@@ -141,17 +115,15 @@ export class ParametrosComponent implements OnInit {
     }
   }
 
-  // Crear un nuevo parámetro
   createParametro(): void {
     this.http.post(`${this.apiUrl}/add`, this.currentParametro).subscribe({
       next: (response: any) => {
         if (response.estado.ack) {
-          console.log('Parámetro creado:', response);
           this.showError('Parámetro creado exitosamente.', false);
           this.loadParametros();
           this.closeModalParametro();
         } else {
-          this.showError(`Error al crear el parámetro: ${response.estado.errDes}`, true);
+          this.showError(`Error al crear el Parámetro: ${response.estado.errDes}`, true);
         }
       },
       error: error => {
@@ -161,83 +133,74 @@ export class ParametrosComponent implements OnInit {
     });
   }
 
-  // Editar un parámetro existente
   updateParametro(): void {
     const url = `${this.apiUrl}/Actualizar/${this.currentParametro.id}`;
-    const updatedData = {
-      nombre: this.currentParametro.nombre,
-      estado: this.currentParametro.estado
-    };
-
     this.http.put<any>(url, this.currentParametro).subscribe({
       next: (response: any) => {
         if (response?.estado?.ack) {
-          console.log('Parámetro actualizado:', response);
           this.showError('Parámetro actualizado exitosamente.', false);
           this.loadParametros();
           this.closeModalParametro();
         } else {
-          this.showError(`Error al actualizar parámetro: ${response?.estado?.errDes || 'Error desconocido'}`, true);
+          this.showError(`Error al actualizar el Parámetro: ${response?.estado?.errDes || 'Error desconocido'}`, true);
         }
       },
       error: (error) => {
         console.error('Error en la solicitud:', error);
-        this.showError('Error en la solicitud al actualizar el parámetro.', true);
+        this.showError('Error en la solicitud al actualizar el Parámetro.', true);
       }
     });
   }
 
-  // Confirmar la eliminación de parámetro
   confirmDelete(id: number): void {
     this.parametroDelete = id;
     this.showModalParametro = false;
     this.showConfirmationDeleteParametro = true;
   }
 
-  // Cerrar el diálogo de confirmación de eliminación
   closeConfirmationDialog(): void {
     this.showConfirmationDeleteParametro = false;
     this.showModalParametro = true;
     this.parametroDelete = null;
   }
 
-// Eliminar parámetro
   deleteParametro(): void {
     if (this.parametroDelete !== null) {
-      this.http.delete<any>(`${this.apiUrl}/${this.parametroDelete}`).subscribe({
+      this.http.delete<any>(`${this.apiUrl}/Eliminar/${this.parametroDelete}`).subscribe({
         next: response => {
           if (response.estado.ack) {
-            console.log('Parámetro eliminado:', response);
             this.showError('Parámetro eliminado exitosamente.', false);
             this.loadParametros();
             this.closeConfirmationDialog();
             this.closeModalParametro();
           } else {
-            this.showError(`Error al eliminar parámetro: ${response.estado.errDes}`, true);
+            this.showError(`Error al eliminar el Parámetro: ${response.estado.errDes}`, true);
           }
         },
         error: error => {
           console.error('Error en la solicitud:', error);
-          this.showError('Error en la solicitud al eliminar el parámetro.', true);
+          this.showError('Error en la solicitud al eliminar el Parámetro.', true);
         }
       });
     }
   }
 
-  // Mostrar un mensaje de error o éxito
   showError(message: string, isError: boolean): void {
     this.errorMessage = { message, isError };
     this.showErrorModal = true;
   }
 
-  // Cerrar el modal de error
   closeErrorModal(): void {
     this.showErrorModal = false;
     this.errorMessage = { message: '', isError: true };
   }
 
-  // Método que se llama cuando cambia la página en el paginador
   onPageChange(event: PageEvent) {
-    this.updatePagedParametros();
+    this.updatePageParametro();
+  }
+
+  getTipoParametroNombre(id: number): string {
+    const tipoParametro = this.tipoParametros.find(tipo => tipo.id === id);
+    return tipoParametro ? tipoParametro.tipO_PARAMETRO : 'Desconocido';
   }
 }
