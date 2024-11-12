db36dc7beed50b51e23016a0e904078dc9f879a5
javierasofia <ja.bermudez@duocuc.cl>
2024-11-12 12:47:57 -0300

    LogIn listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,28 +17,30 @@ import { GestionTipoParametrosComponent } from './gestion-tipo-parametros/gestio
 import { ParametrosComponent } from './parametros/parametros.component';
 // Login
 import { LogInComponent } from './log-in/log-in.component';
+// Protección de rutas
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   // Aqui están las rutas de las páginas
   {path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirige a la ruta de login por defecto
-  {path: 'dashboard', component: DashboardComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   // Ejemplo {path: 'settings', component: SettingsComponent} // Recordar cambiar el nombre de su component, por ejemplo: SettingsComponent
-  {path: 'actas', component: ActasComponent},
-  {path: 'perfiles', component: PerfilesComponent},
-  {path: 'obras', component: ObrasComponent},
-  {path: 'gestion-tarea', component: GestionTareaComponent},
+  {path: 'actas', component: ActasComponent, canActivate: [AuthGuard]},
+  {path: 'perfiles', component: PerfilesComponent, canActivate: [AuthGuard]},
+  {path: 'obras', component: ObrasComponent, canActivate: [AuthGuard]},
+  {path: 'gestion-tarea', component: GestionTareaComponent, canActivate: [AuthGuard]},
   // Creación de usuarios
-  { path: 'crear-usuario', component: CrearUsuariosComponent },
+  { path: 'crear-usuario', component: CrearUsuariosComponent, canActivate: [AuthGuard]},
   // Edición de usuarios
-  { path: 'edicion-de-usuarios', component: EdicionDeUsuariosComponent },
+  { path: 'edicion-de-usuarios', component: EdicionDeUsuariosComponent, canActivate: [AuthGuard] },
   // Gestión de proveedores
-  { path: 'gestion-proveedores', component: GestionProveedoresComponent },
+  { path: 'gestion-proveedores', component: GestionProveedoresComponent, canActivate: [AuthGuard] },
   // Gestión de especialidad
-  { path: 'gestion-especialidad', component: GestionEspecialidadComponent },
+  { path: 'gestion-especialidad', component: GestionEspecialidadComponent, canActivate: [AuthGuard] },
   // Gestion tipo de parámetro
-  { path: 'gestion-tipo-parametros', component: GestionTipoParametrosComponent },
+  { path: 'gestion-tipo-parametros', component: GestionTipoParametrosComponent, canActivate: [AuthGuard] },
   // Gestión parámetros
-  { path: 'parametros', component: ParametrosComponent },
+  { path: 'parametros', component: ParametrosComponent, canActivate: [AuthGuard] },
   // LogIn debe ser la primera página que se ejecuta
   { path: 'login', component: LogInComponent }, // Ruta para el componente de inicio de sesión
   { path: '**', redirectTo: '/login' } // Redirige rutas no encontradas a login
